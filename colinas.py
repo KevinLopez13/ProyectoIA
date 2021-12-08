8506745dc876e08e2adb19409d65803babfc8186
KevinLopez13 <enanodeoz@gmail.com>
2021-12-07 22:27:26 -0600

    Código funcional.

--- colinas.py
+++ colinas.py
@@ -5,15 +5,11 @@ class Nodo():
 		self.valor = valor
 		self.hijos = []
 
-	def addHijo(self, h):
-		h.padre = self.nombre
-		self.hijo.append(h)
-
-	def addHijos(self,**kwards):
-		for hijo in kwards.items():
+	def addHijos(self, *args):
+		for hijo in args:
 			self.hijos.append(hijo)
 
-	def getMin(self):
+	def getBest(self):
 		if self.hijos:
 			self.hijos.sort( key=lambda v: v.valor )
 			return self.hijos.pop(0)
@@ -23,75 +19,21 @@ class Nodo():
 	def __str__(self) -> str:
 		return self.nombre
 
-	def printH(self):
-		print("Padre:",self.nombre)
-		for i in self.hijos:
-			print(i)
-
 class Grafo:
 	def __init__(self) -> None:
 		self.nodos = {}
 
-	def addNodo(self, n):
-		self.nodos[n.nombre] = n
+	def addNodos(self, *args):
+		for n in args:
+			self.nodos[n.nombre] = n
 
-	def solucion(self, nodo, nodolst):
-		padre = nodo.getPadre()
-		nodoNext = nodo
-		tmplst = []
-		while padre != None:
-			tmplst.append(padre)
-
-			padre = padre.getPadre()
-
-	def colinas(self,raiz, meta):
-		colaNodos = []
-		respuesta = []
-		actualN = raiz
-		
-		
-		while True:
-			# Guardamos las otras opciones posibles
-			if actualN.hijos:
-				if actualN not in colaNodos:
-					colaNodos.append(actualN)
-			else:
-				if colaNodos[0].hijos:
-					actualN = colaNodos[0]
-					respuesta.pop()
-				else:
-					colaNodos.pop(0)
-					actualN = colaNodos[0]
-
-			# Obtenemos el hijo con mejor valor y actualizamos
-			#if actualN not in respuesta:
-			respuesta.append(actualN)
-			actualN = actualN.getMin()
-				
-			# Ya es nuestra solucion?
-			if actualN.nombre == meta.nombre:
-				respuesta.append(actualN)
-				for n in respuesta:
-					print(n)
-				return True
-
-			# si llegamos a una hoja
-			elif not actualN.hijos:
-				respuesta.pop()
-			else:
-				pass
-
-			if not colaNodos:
-				print('!Meta')
-				return False
-
-	def colinas2(self, raiz, meta):
+	def colinas(self, raiz, meta):
 		colaNodos = []
 		respuesta = []
 		actualN = raiz
 
 		while True:
-			mejorHijo = actualN.getMin()
+			mejorHijo = actualN.getBest()
 			print("Actual:", actualN,'-> Mejor hijo:', mejorHijo,'\n')
 
 			if actualN.hijos and actualN not in colaNodos:
@@ -102,25 +44,23 @@ class Grafo:
 			if mejorHijo != None:
 				if actualN not in respuesta:
 					respuesta.append(actualN)
-				#respuesta.append(mejorHijo)
+
 				actualN = mejorHijo
 
 				# Vemos si es hoja
 				if not actualN.hijos:
 					if actualN.nombre == meta.nombre:
 						respuesta.append(actualN)
-						for n in respuesta:
-							print(n)
-						return True
+						res = 'El mejor camino es: '
+						for r in respuesta:
+							res = res + str(r) + ' -> '
+						return res[:-4]
 					else:
-						actualN = colaNodos[0]
-						respuesta = [raiz]
-			else:
-				print("None:")
-				actualN = colaNodos[0]
-				respuesta = [raiz]
-
-
+						if colaNodos:
+							actualN = colaNodos[0]
+							respuesta = [raiz]
+						else:
+							return "No existe solución"
 
 
 
@@ -136,52 +76,18 @@ H = Nodo('H',11)
 J = Nodo('J',20)
 Z = Nodo('Z',0)
 
-I.hijos.append(A)
-I.hijos.append(B)
-I.hijos.append(C)
-I.hijos.append(D)
+I.addHijos(A,B,C,D)
+B.addHijos(E,F)
+D.addHijos(F,J)
+E.addHijos(G)
+F.addHijos(H)
+H.addHijos(Z)
 
-#A.hijos.append(B)
-
-#B.addHijos(A,C,E,F)
-#B.hijos.append(A)
-#B.hijos.append(C)
-B.hijos.append(E)
-B.hijos.append(F)
-
-#C.hijos.append(B)
-
-D.hijos.append(F)
-D.hijos.append(J)
-
-E.hijos.append(G)
-
-F.hijos.append(H)
-
-H.hijos.append(Z)
+X = Nodo('X',40)
 
 g = Grafo()
+g.addNodos(A,B,C,D,E,F,G,H,I,J,Z)
 
-g.addNodo(A)
-g.addNodo(B)
-g.addNodo(C)
-g.addNodo(D)
-g.addNodo(E)
-g.addNodo(F)
-g.addNodo(G)
-g.addNodo(H)
-g.addNodo(I)
-g.addNodo(J)
-g.addNodo(Z)
-
-print(g.colinas2(I,C))
-
-
-
-			
-
-
-
-
-	
+r = g.colinas(I,J)
+print(r)
 
